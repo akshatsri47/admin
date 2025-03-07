3c9ec3f7dfc1a6b169c19280dd98bb4e6135032c
akshatsri47 <ak.sr2201@gmail.com>
2025-03-08 01:14:54 +0530

    fix

--- src/app/api/signature/route.ts
+++ src/app/api/signature/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 import cloudinary from "../../../../utils/cloudinary";
 export async function GET( ) {
   const timestamp = Math.round(new Date().getTime() / 1000);
